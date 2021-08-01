ff5f95dba465a760a7f8475b9047c441fd3c95a1
Jakobg1215 <howtoblockcitywars@gmail.com>
2021-07-31 20:50:07 -0500

    add protocol id data

---
+++ src/utils/Registry.ts
@@ -0,0 +1,9 @@
+import blocks from './data/blocks.json';
+import entities from './data/entities.json';
+import items from './data/items.json';
+
+export default class Registry {
+    static getBlockId() {}
+    static getItemId() {}
+    static getEntityId() {}
+}
