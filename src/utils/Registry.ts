3f3c7150c59ec55234f09a5e60d6d9fff8bf7fb0
Jakobg1215 <howtoblockcitywars@gmail.com>
2021-08-08 09:43:12 -0500

    Add Registry

--- src/utils/Registry.ts
+++ src/utils/Registry.ts
@@ -3,7 +3,18 @@ import entities from './data/entities.json';
 import items from './data/items.json';
 
 export default class Registry {
-    static getBlockId() {}
-    static getItemId() {}
-    static getEntityId() {}
+    private static blocks = Object.entries(blocks);
+    private static items = Object.entries(items);
+    private static entities = Object.entries(entities);
+    public static getBlockId(block: string) {
+        const blockstates: any | undefined = this.blocks.find(p => p[0] === `minecraft:${block}`)?.[1];
+        if (!blockstates) return null;
+        return blockstates.states.find((p: any) => p.default === true)?.id ?? null;
+    }
+    public static getItemId(item: string) {
+        return this.items.find(p => p[0] === `minecraft:${item}`)?.[1].protocol_id ?? null;
+    }
+    public static getEntityId(entity: string) {
+        return this.entities.find(p => p[0] === `minecraft:${entity}`)?.[1].protocol_id ?? null;
+    }
 }
