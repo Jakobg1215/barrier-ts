e49abc4206e0ff531610fbdb6e62d97f8482f48d
Jakobg1215 <howtoblockcitywars@gmail.com>
2021-11-21 02:10:22 -0600

    Add Swing Handler

--- src/networking/handlers/game/SwingHandler.ts
+++ src/networking/handlers/game/SwingHandler.ts
@@ -1,10 +1,15 @@
 import type BarrierTs from '../../../BarrierTs';
+import { InteractionHand } from '../../../types/enums/InteractionHand';
 import type Connection from '../../Connection';
+import ClientboundAnimatePacket from '../../packets/game/ClientboundAnimatePacket';
 import type ServerboundSwingPacket from '../../packets/game/ServerboundSwingPacket';
 import type Handler from '../Handler';
 
 export default class SwingHandler implements Handler<ServerboundSwingPacket> {
-    public hander(_packet: ServerboundSwingPacket, _connection: Connection, _server: BarrierTs): void {
-        throw new Error('Method not implemented.');
+    public hander(packet: ServerboundSwingPacket, connection: Connection, server: BarrierTs): void {
+        if (packet.hand === InteractionHand.MAIN_HAND) {
+            return server.brodcast(new ClientboundAnimatePacket(connection.player.id, 0), [connection.player.id]);
+        }
+        server.brodcast(new ClientboundAnimatePacket(connection.player.id, 3), [connection.player.id]);
     }
 }
